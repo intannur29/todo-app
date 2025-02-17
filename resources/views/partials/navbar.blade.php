84dc2ed64ea757b5e6dd5d42c63e4f08847e6dad
intannur29 <intannurrahmawati00@gmail.com>
2025-02-17 10:25:09 +0700

    menambahkan profil di navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,42 +1,23 @@
 <nav class="navbar navbar-expand-lg bg-secondary navbar-dark fixed-top">
     <div class="container d-flex justify-content-between">
+        <!-- Form pencarian -->
         <form class="d-flex" action="{{ route('search') }}" method="GET" role="search">
             <input id="search-input" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-black" type="submit">Refresh</button>
         </form>
+
+        <!-- Nama aplikasi -->
         <a class="navbar-brand fw-bolder" href="#">{{ config('app.name') }}</a>
-        <button type="button" class="btn btn-outline-dark flex-shrink-0" style="width: 18rem; height: fit-content;"<nav class="navbar navbar-expand-lg bg-secondary navbar-dark fixed-top">
-            <!-- Container untuk elemen-elemen di dalam navbar -->
-            <div class="container d-flex justify-content-between">
-                
-                <!-- Form pencarian -->
-                <form class="d-flex" action="{{ route('search') }}" method="GET" role="search">
-                    <!-- Input pencarian -->
-                    <input id="search-input" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <!-- Tombol untuk mengirimkan pencarian -->
-                    <button class="btn btn-outline-black" type="submit">Refresh</button>
-                </form>
-                
-                <!-- Link untuk nama aplikasi, akan mengarah ke halaman utama -->
-                <a class="navbar-brand fw-bolder" href="#">{{ config('app.name') }}</a>
-                
-                <!-- Tombol untuk membuka modal dan menambahkan item baru -->
-                <button type="button" class="btn btn-outline-dark flex-shrink-0" style="width: 18rem; height: fit-content;"
-                data-bs-toggle="modal" data-bs-target="#addListModal">
-                    <!-- Ikon plus dan teks "Tambah" di dalam tombol -->
-                    <span class="d-flex align-items-center justify-content-center">
-                        <i class="bi bi-plus fs-5"></i>
-                        Tambah
-                    </span>
-                </button>
-            </div>
-        </nav>
-        
-        data-bs-toggle="modal" data-bs-target="#addListModal">
-        <span class="d-flex align-items-center justify-content-center">
-            <i class="bi bi-plus fs-5"></i>
-            Tambah
-        </span>
-    </button>
+
+        <!-- Dropdown untuk foto profil -->
+        <div class="nav-item dropdown">
+            <a href="#" class="nav-link">
+                <!-- Gambar profil -->
+                <img class="rounded-circle me-lg-2" src="{{ asset('image/fotoijazahintan.jpeg') }}" alt="Foto Profil"
+                    style="width: 40px; height: 40px: object-fit: cover;" />
+                <!-- Nama pengguna (hanya ditampilkan di layar besar) -->
+                <span class="d-none d-lg-inline-flex">Intan Nur Rahmawati</span>
+            </a>
+        </div>
     </div>
 </nav>
