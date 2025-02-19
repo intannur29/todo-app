137571786ea8169b9633ef5abeabef49b51bb229
intannur29 <intannurrahmawati00@gmail.com>
2025-02-19 09:50:04 +0700

    mengubah warna border di biodata

--- resources/views/biodata/create.blade.php
+++ resources/views/biodata/create.blade.php
@@ -31,8 +31,8 @@
             color: #fff;
             text-align: center;
             padding: 50px 35px;
-            border: 1px solid rgba(255, 255, 255, 0.3);
-            background: rgba(255, 255, 255, 0.2);
+            border: 1px solid rgba(10, 10, 10, 0.3);
+            background: rgba(196, 193, 193, 0.027);
             border-radius: 360x;
             box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
             backdrop-filter: blur(5px);
